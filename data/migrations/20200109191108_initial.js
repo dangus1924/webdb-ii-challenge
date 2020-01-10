0d9c354575ffe2e601911d2a75a7fc7cbe8e1e2c
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-09 20:36:13 -0500

    added seed

--- data/migrations/20200109191108_initial.js
+++ data/migrations/20200109191108_initial.js
@@ -9,7 +9,8 @@ exports.up = async function(knex) {
   await knex.schema.createTable('title', (table) => {
     table.increments('id')
     table.string('status')        
-        .notNullable();    
+        .notNullable()
+            
   });
   await knex.schema.createTable('cars', (table) => {
     table.increments('id')
